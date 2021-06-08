1ec3c93a3a8f451c3d2c76f27cf7363bd573faaa
Juneus Magsakay <jbm9397@gmail.com>
2021-06-08 01:51:32 -0400

    initial commit

---
+++ FizzBuzz.java
@@ -0,0 +1,17 @@
+public class FizzBuzz {
+    public String fizzBuzz(int number) { 
+        if (number % 15 == 0){
+            return "FizzBuzz";
+        }
+        else if (number % 3 == 0){
+            return "Fizz";
+        }
+        else if(number % 5 == 0){
+            return "Buzz";
+        }
+        else {
+            return Integer.toString(number);
+        }
+    }
+}
+
